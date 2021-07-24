4ddf9b89910a8c06b10d79422113c515e6c1affc
ashley-m0 <ashley.mojica@knights.ucf.edu>
2021-07-24 17:40:15 -0400

    Update 3 - Add and Implement Search Elements

--- src/main/java/ucf/assignments/Item.java
+++ src/main/java/ucf/assignments/Item.java
@@ -1,38 +1,118 @@
 package ucf.assignments;
 
+import java.math.BigDecimal;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Item {
     private String value;
     private String serialNumber;
     private String name;
 
-    public Item(String value, String serialNumber, String name){
-        this.value = value;
-        this.serialNumber = serialNumber;
-        this.name = name;
+    public boolean setValue(String value){
+        //if the value fits the requirements
+        if (authenticateValue(value)){
+            //Format the String to ensure that it has decimal values
+            Double valueDouble = Double.valueOf(value);
+            String valueString = String.valueOf(valueDouble);
+            //Assign the String with a "$" in front
+            this.value = "$" + valueString;
+            //return true to indicate that the value was added successfully
+            return true;
+        }else{
+            //return false to indicate an incorrect value
+            return false;
+        }
     }
 
-    public void setValue(double value){
-        String num = Double.toString(value);
-        this.value = "$" + num;
+    public boolean authenticateValue(String value){
+        Double num = Double.valueOf(value);
+        if (BigDecimal.valueOf(num).scale() > 2){
+            return false;
+        }else{
+            return true;
+        }
     }
 
     public String getValue(){
         return value;
     }
 
-    public void setSerialNumber(String serialNumber){
-        this.serialNumber = serialNumber;
+    public boolean setSerialNumber(String serialNumber){
+        //if serial number fits the requirements
+        if(authenticateSerialNumber(serialNumber)){
+            //assign serial number
+            this.serialNumber = serialNumber;
+            //return true to indicate that the serialNumber was added successfully
+            return true;
+        }else{
+            //returns false to indicate an incorrect serial number
+            return false;
+        }
+
+    }
+
+    public boolean authenticateSerialNumber(String serialNumber){
+        if(correctLength(serialNumber) && !specialCharactersPresent(serialNumber)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean correctLength(String serialNumber){
+        //transform string into a character array
+        char[] numbers = serialNumber.toCharArray();
+        //if the array's length is longer than 10 return false and return true otherwise
+        if (numbers.length != 10){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public boolean specialCharactersPresent(String serialNumber){
+        //initialize pattern to see if any special characters were entered
+        Pattern p = Pattern.compile("[^A-Za-z0-9]");
+        //initialize matcher to the string
+        Matcher m = p.matcher(serialNumber);
+        //returns true if there is a special character in the string; false otherwise
+        return m.find();
     }
 
     public String getSerialNumber(){
         return serialNumber;
     }
 
-    public void setName(String name){
-        this.name = name;
+    public boolean setName(String name){
+        //if name fits the requirements
+        if(authenticateName(name)){
+            //assign name
+            this.name = name;
+            //return true to indicate that the name was added successfully
+            return true;
+        }else{
+            //return false to indicate an incorrect name
+            return false;
+        }
+
+    }
+
+    public boolean authenticateName(String name){
+        char[] letters = name.toCharArray();
+        if(letters.length >= 2 && letters.length <= 256){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     public String getName(){
         return name;
     }
+
+
+
+
+
 }
--- src/main/java/ucf/assignments/MainScreenController.java
+++ src/main/java/ucf/assignments/MainScreenController.java
@@ -43,45 +43,13 @@ public class MainScreenController implements Initializable {
     @FXML private TextField valueField;
     @FXML private TextField serialNumberField;
     @FXML private TextField nameField;
+    @FXML private TextField queryField;
+
+    //Initialize Choice Box
+    @FXML private ChoiceBox queryType;
 
     //Initialize Text Area for system messages
     @FXML private TextArea systemMessageArea;
-
-    public void updateTable(ArrayList<Item> currList){
-        //initialize the cell values of the columns
-        valueColumn.setCellValueFactory(new PropertyValueFactory<Item, String>("value"));
-        serialNumberColumn.setCellValueFactory(new PropertyValueFactory<Item, String>("serialNumber"));
-        nameColumn.setCellValueFactory(new PropertyValueFactory<Item, String>("name"));
-
-        //set description column to be able to be typed in when user clicks on a cell
-        valueColumn.setCellFactory(TextFieldTableCell.forTableColumn());
-        serialNumberColumn.setCellFactory(TextFieldTableCell.forTableColumn());
-        nameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
-
-        //set the items in the table by calling the getAllTaskInfo function
-        itemTable.setItems(getTableInfo(currList));
-    }
-
-    public ObservableList<Item> getTableInfo(ArrayList<Item> currList){
-        //initialize an ObservableList that will contain the information that will be added into the table
-        ObservableList<Item> listInfo = FXCollections.observableArrayList();
-        //for loop that iterates from 0 to the size of the taskList
-        for(int i = 0; i < mainList.size(); i++){
-            //add Task to the ObservableList
-            listInfo.add(mainList.get(i));
-        }
-        //return ObservableList
-        return listInfo;
-    }
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        //call updateTable function
-        updateTable(mainList);
-        //set table to be editable and all for items to be selected
-        itemTable.setEditable(true);
-        itemTable.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-    }
 
 
     /**
@@ -119,25 +87,173 @@ public class MainScreenController implements Initializable {
     public void addItemButtonPressed(ActionEvent actionEvent) {
         //call add item method
         mainList = addItem(mainList);
+
         //update table
         updateTable(mainList);
     }
 
     public ArrayList<Item> addItem(ArrayList<Item> currList){
-        //call method to get user input from text fields
-        Item newItem = getInfoFromFields();
-        //add item to currList
-        currList.add(newItem);
+        //get information from fields
+        String[] information = getInfoFromFields();
+        //initialize a boolean variable to keep track of the correct values
+        boolean correct = true;
+        //initialize the new item
+        Item newItem = new Item();
+        //initialize the message that will be displayed to the user
+        String message = "";
+        if(!newItem.setValue(information[0])){
+            message += "Please enter a valid value.(Only values with two or less decimal places)\n";
+            correct = false;
+        }
+        if(!newItem.setSerialNumber(information[1])){
+            message += "Please enter a valid serial number. (10 characters long, no special character)\n";
+            correct = false;
+        }
+        if(!newItem.setName(information[2])){
+            message += "Please enter a valid name. (At least 2 characters long but smaller than 256 characters long)\n";
+            correct = false;
+        }
+        if (correct){
+            //add item to currList
+            currList.add(newItem);
+            //indicate to user that the item was successfully added
+            message += "Item successfully added.";
+            //clear the text fields
+            resetFields();
+        }
+        //set text area to the message created
+        systemMessageArea.setText(message);
         //return currList
         return currList;
     }
 
-    public Item getInfoFromFields(){
-        String value = valueField.getText();
-        String serialNumber = serialNumberField.getText();
-        String name = nameField.getText();
-        Item newItem = new Item(value, serialNumber, name);
-        return newItem;
+    public String[] getInfoFromFields(){
+        //initialize String array that will hold all information of new Item
+        String[] information = new String[3];
+        //get information from fields and place them in the array
+        information[0] = valueField.getText();
+        information[1] = serialNumberField.getText();
+        information[2] = nameField.getText();
+        //return String array
+        return information;
     }
 
+
+    public void searchItemButtonPressed(ActionEvent actionEvent) {
+        //call the searchItem method to find whether the item was found or not
+        String message = searchItem(mainList, queryType.getValue().toString(), queryField.getText());
+        //set the system message to equal the message received
+        systemMessageArea.setText(message);
+        //reset all fields
+        resetFields();
+    }
+
+    public String searchItem(ArrayList<Item> currList, String type, String query){
+        //initialize search index
+        int index = -1;
+        //initialize return message
+        String message = "";
+        //if the query is by the serial number
+        if (type.equals("Serial Number")){
+            //call the searchBySN method
+            index = searchBySN(currList, query);
+        }
+        //if the query is by name
+        if (type.equals("Name")){
+            //call the searchByName method
+            index = searchByName(currList, query);
+        }
+        //if the query was not found
+        if (index == -1){
+            //indicate in message that item was not found
+            message += "Item not found.";
+        }else{
+            //indicate in message that the item was found and print the item's information
+            message += String.format("Item found:\nValue: %10s | Serial Number: %12s | Name: %s", currList.get(index).getValue(), currList.get(index).getSerialNumber(), currList.get(index).getName());
+        }
+        //return message
+        return message;
+    }
+
+    public int searchBySN(ArrayList<Item> currlist, String query){
+        //iterate through ArrayList of Items
+        for(int i = 0; i < currlist.size(); i++){
+            //if an item's serial number matches the query
+            if (currlist.get(i).getSerialNumber().equals(query)){
+                //return the index
+                return i;
+            }
+        }
+        //if the query was not found return a -1
+        return -1;
+    }
+
+    public int searchByName(ArrayList<Item> currlist, String query){
+        //iterate through ArrayList of Items
+        for(int i = 0; i < currlist.size(); i++){
+            //if an item's name matches the query
+            if (currlist.get(i).getName().equals(query)){
+                //return the index
+                return i;
+            }
+        }
+        //if the query was not found return a -1
+        return -1;
+    }
+
+    public void setUpChoiceBox(){
+        //add Serial Number and Name to the ChoiceBox options
+        queryType.getItems().add("Serial Number");
+        queryType.getItems().add("Name");
+        //set the default choice to be Serial Number
+        queryType.setValue("Serial Number");
+    }
+
+    public void resetFields(){
+        //clear all TextFields
+        valueField.clear();
+        serialNumberField.clear();
+        nameField.clear();
+        queryField.clear();
+        //reset ChoiceBox back to default "Serial Number"
+        queryType.setValue("Serial Number");
+    }
+
+    public void updateTable(ArrayList<Item> currList){
+        //initialize the cell values of the columns
+        valueColumn.setCellValueFactory(new PropertyValueFactory<Item, String>("value"));
+        serialNumberColumn.setCellValueFactory(new PropertyValueFactory<Item, String>("serialNumber"));
+        nameColumn.setCellValueFactory(new PropertyValueFactory<Item, String>("name"));
+
+        //set description column to be able to be typed in when user clicks on a cell
+        valueColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        serialNumberColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        nameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        //set the items in the table by calling the getAllTaskInfo function
+        itemTable.setItems(getTableInfo(currList));
+    }
+
+    public ObservableList<Item> getTableInfo(ArrayList<Item> currList){
+        //initialize an ObservableList that will contain the information that will be added into the table
+        ObservableList<Item> listInfo = FXCollections.observableArrayList();
+        //for loop that iterates from 0 to the size of the taskList
+        for(int i = 0; i < mainList.size(); i++){
+            //add Task to the ObservableList
+            listInfo.add(mainList.get(i));
+        }
+        //return ObservableList
+        return listInfo;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //call updateTable function
+        updateTable(mainList);
+        //set choice box options
+        setUpChoiceBox();
+        //set table to be editable and all for items to be selected
+        itemTable.setEditable(true);
+        itemTable.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+    }
 }
